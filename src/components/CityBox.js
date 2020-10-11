0f0f401e4987ad1312c20506964856eb3b66b4af
José Julio Villanueva Rodríguez <julio.villanueva@udc.es>
2020-10-11 20:29:14 +0200

    citybox 0.0.0

---
+++ src/components/CityBox.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function CityBox () {
+  const [city, setCity] = useState("")
+
+  const getCity = () => {
+    setCity("")
+  }
+};
+
+export default CityBox;
