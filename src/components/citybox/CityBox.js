55d701f6b97bb1c61a48ddf64f8ef2683f615fa8
José Julio Villanueva Rodríguez <julio.villanueva@udc.es>
2020-10-19 13:53:26 +0200

    Actualmente intentando dejar el CityBox correctamente (columnas con el mismo tamaño)

--- src/components/citybox/CityBox.js
+++ src/components/citybox/CityBox.js
@@ -1,30 +1,54 @@
 import React from "react";
 import { Col, Card, Row } from "react-bootstrap";
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage } from "react-intl";
 import "./CityBox.css";
 
 class CityBox extends React.Component {
   render() {
     return (
-      <Card className="cityBox" style={{backgroundColor:'#AB918C' }}>
-        <Card.Header className="cHeader">{this.props.weatherCityName} 
-        <img className="weatherIcon" alt="Weather icon matching current weather" src ="http://openweathermap.org/img/wn/10d@2x.png"/>
+      <Card 
+        md="auto"
+        className="cityBox"
+        style={{ backgroundColor: "#AB918C" }}
+      >
+        <Card.Header className="cHeader">
+          {this.props.weatherCityName}
+          <img
+            className="weatherIcon"
+            alt="Weather icon matching current weather"
+            src="http://openweathermap.org/img/wn/10d@2x.png"
+          />
         </Card.Header>
-        <Row className="cabecera">
-          <Col><FormattedMessage id='citybox.description'></FormattedMessage></Col>
-          <Col><FormattedMessage id='citybox.windspeed'></FormattedMessage></Col>
+        <Card.Body className="justify-content-md-center">
+          <Row>
+            <Col>
+              <Row className="cabecera">
+                <FormattedMessage id="citybox.description"></FormattedMessage>
+              </Row>
+              <Row className="justify-content-md-center">{this.props.weatherDescription}</Row>
+            </Col>
+            <Col md="auto">
+              <Row className="cabecera">
+                <FormattedMessage id="citybox.windspeed"></FormattedMessage>
+              </Row>
+              <Row className="justify-content-md-center"> {this.props.weatherWindSpeed} m/s</Row>
+            </Col>
           </Row>
-        <Row>
-          <Col>{this.props.weatherDescription}</Col>
-          <Col>{this.props.weatherWindSpeed} m/s</Col>
-        </Row>
-        <Row className="cabecera">
-          <Col><FormattedMessage id='citybox.temperature'></FormattedMessage></Col>
-          <Col md="auto"><FormattedMessage id='citybox.weatherfeelslike'></FormattedMessage></Col></Row>
-        <Row>
-          <Col>{this.props.weatherTemp} ºC</Col>
-          <Col>{this.props.weatherFeelsLike} ºC</Col>
-        </Row>
+          <Row>
+            <Col>
+              <Row className="cabecera">
+                <FormattedMessage id="citybox.temperature"></FormattedMessage>
+              </Row>
+              <Row  className="justify-content-md-center">{this.props.weatherTemp} ºC</Row>
+            </Col>
+            <Col md="auto">
+              <Row className="cabecera">
+                <FormattedMessage id="citybox.weatherfeelslike"></FormattedMessage>
+              </Row>
+              <Row className="justify-content-md-center">{this.props.weatherFeelsLike} ºC</Row>
+            </Col>
+          </Row>
+        </Card.Body>
       </Card>
     );
   }
